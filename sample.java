4555e17f556f94bdc7a50be0d636cbbe4ce7a19d
Karthikeyan M <122173451+mkarthi1796@users.noreply.github.com>
2023-02-01 15:36:24 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,9 @@
 public class sample {
 
 	public static void main(String[] args) {
-		System.out.println("sample program of java-for jenkins");
+		System.out.println("sample program 1");
+		System.out.println("sample program 2");
+		System.out.println("sample program 3");
 	}
 
 }
